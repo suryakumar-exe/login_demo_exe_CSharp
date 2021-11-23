71787b2ba19cc625256bd1189206a4366f0399a2
suryakumar-exe <suryakumar2808@gmail.com>
2021-11-23 10:16:45 +0530

    2nd c

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -176,7 +176,7 @@ namespace WindowsFormsApp1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1264, 1061);
+            this.ClientSize = new System.Drawing.Size(1264, 749);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
